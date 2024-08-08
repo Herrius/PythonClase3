a24721c8109e51cff24d612695573492bf545cd9
Enrique Ubaldo <65200014+Herrius@users.noreply.github.com>
2024-08-08 12:11:56 -0500

    update ejercicio con solo print

--- 1.1 ejercicio.py
+++ 1.1 ejercicio.py
@@ -3,13 +3,3 @@
 # Descripción:
 # Escribe un programa que utilice la función print para mostrar tu nombre, edad, y ciudad en una sola línea, separados por comas.
 # Utiliza variables para almacenar cada uno de estos datos antes de imprimirlos.
-
-
-
-# Ejercicio Intermedio
-# Objetivo: Practicar la formateación de cadenas con print.
-# Descripción:
-# Escribe un programa que solicite al usuario que introduzca su nombre y su año de nacimiento.
-# Luego, el programa debe calcular la edad del usuario y mostrar un mensaje personalizado que diga:
-# "Hola [nombre], tienes [edad] años de edad." Utiliza el método de formateo de cadenas que prefieras
-# (f-string, format() o concatenación).
